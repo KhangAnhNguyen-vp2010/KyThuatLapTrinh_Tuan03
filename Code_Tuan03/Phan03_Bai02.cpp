fd49aa5ee7c286cc921ef806000ab31604ab0e93
Anh Khang <taicuoi664@gmail.com>
2024-07-26 20:39:11 +0700

    Add Phan03_Bai02_Cau03_XuatCacPhanTuThuocDuongCheoSongSongVoiDuongCheoChinh

--- Code_Tuan03/Phan03_Bai02.cpp
+++ Code_Tuan03/Phan03_Bai02.cpp
@@ -37,10 +37,34 @@ void xuat_Mang(int a[][MAXSIZE], int n)
 void xuatPT_DuongCheoChinh(int a[][MAXSIZE], int n)
 {
     printf("Cac phan tu tren duong cheo chinh la:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+	{
         printf("%d ", a[i][i]);
     }
     printf("\n");
+}
+
+void xuatPT_DuongCheoSongSong(int a[][MAXSIZE], int n)
+{
+    // Các đường chéo song song nằm trên đường chéo chính (bao gồm cả đường chéo chính)
+    for (int k = 0; k < n; k++)
+	{
+        printf("Cac phan tu cua duong cheo bat dau tu (0, %d):\n", k);
+        for (int i = 0, j = k; i < n && j < n; i++, j++) 
+		{
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Các đường chéo song song nằm dưới đường chéo chính
+    for (int k = 1; k < n; k++) {
+        printf("Cac phan tu cua duong cheo bat dau tu (%d, 0):\n", k);
+        for (int i = k, j = 0; i < n && j < n; i++, j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main()
@@ -51,6 +75,8 @@ int main()
 	xuat_Mang(a,n);
 	printf("\n----------------------------------\n");
 	xuatPT_DuongCheoChinh(a,n);
+	printf("\n----------------------------------\n");
+	xuatPT_DuongCheoSongSong(a,n);
 	getch();
 	return 0;
 }
