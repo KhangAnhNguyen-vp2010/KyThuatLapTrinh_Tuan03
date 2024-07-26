7daa53b41ce90ed818b628a871bd988208c9b7ab
Anh Khang <taicuoi664@gmail.com>
2024-07-26 17:34:36 +0700

    Add Phan03_Bai02_TaoMangCapN

---
+++ Code_Tuan03/Phan03_Bai02.cpp
@@ -0,0 +1,46 @@
+﻿#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+#define MAXSIZE 100
+
+void tao_MangCapN(int a[][MAXSIZE], int &n)
+{
+	printf("Nhap n:");
+	scanf("%d", &n);
+	while (n<5)
+	{
+		printf("n phai >= 5!!!\n");
+		printf("Nhap lai n:");
+		scanf("%d", &n);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			a[i][j]=rand()%10;
+		}
+	}
+}
+
+void xuat_Mang(int a[][MAXSIZE], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+}
+
+
+int main()
+{
+	int a[MAXSIZE][MAXSIZE];
+	int n;
+	tao_MangCapN(a,n);
+	xuat_Mang(a,n);
+	getch();
+	return 0;
+}
